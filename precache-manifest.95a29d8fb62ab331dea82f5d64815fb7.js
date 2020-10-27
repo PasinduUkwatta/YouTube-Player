cbdc7d5cbe6c4d5ad13683073c68c8a8a405b919
Pasindu Thiwanka <pasindu.17@itfac.mrt.ac.lk>
2020-10-27 12:38:31 +0530

    Updates

---
+++ precache-manifest.95a29d8fb62ab331dea82f5d64815fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0887dfdc0eb3b404ced749735d3a9c",
+    "url": "/YouTube-Player/index.html"
+  },
+  {
+    "revision": "584c67accfe560501120",
+    "url": "/YouTube-Player/static/css/main.248b0c78.chunk.css"
+  },
+  {
+    "revision": "718af6a4072be147e7fa",
+    "url": "/YouTube-Player/static/js/2.bfdc24ba.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/YouTube-Player/static/js/2.bfdc24ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584c67accfe560501120",
+    "url": "/YouTube-Player/static/js/main.b5a40d38.chunk.js"
+  },
+  {
+    "revision": "df07b09749ed1c89af89",
+    "url": "/YouTube-Player/static/js/runtime-main.d481d9f3.js"
+  }
+]);
