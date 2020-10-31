ac4dac8cbadbda2bfa13127d783527722634928b
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-10-31 09:12:33 +0530

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const KEY ="AIzaSyB5FHluCvC0PzHr3UQ5W2wDJhdU-Xfoxxw"
+const KEY ="AIzaSyBpuy4PTjd7GZflbAUnOxtPyg6IlKDe3yA"
 
 export default axios.create({
     baseURL:"https://www.googleapis.com/youtube/v3",
